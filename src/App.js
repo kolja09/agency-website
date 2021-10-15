d1029be60acfd605d73d927ffca5c50c92bab812
kolja09 <kolja9062005@gmail.com>
2021-10-15 21:15:46 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {GlobalStyle} from "./globalStyles";
-import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./Pages/Home";
 
 
@@ -8,9 +7,7 @@ const App = () => {
     return (
         <>
             <GlobalStyle/>
-            <Router>
-                <Route path='/' exact component={Home}/>
-            </Router>
+            <Home/>
         </>
     );
 };
